ff31188be7b510cbc0a6568f12acb9a84d7ecae4
ryutah <ryuta0221@gmail.com>
2018-07-02 12:40:34 +0900

    Add: map, map_err functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,39 @@
 //! futures-rs copy
 #![no_std]
 
+#[cfg(feature = "use_std")]
+extern crate std;
+
 #[macro_use]
 extern crate log;
 
 pub mod poll;
 use poll::{Async, Poll};
 
+mod map;
+use map::Map;
+
+mod map_err;
+use map_err::MapErr;
+
 macro_rules! if_std {
     ($($i:item)*) => ($(
-        #[cfg(future = "use_std")]
+        #[cfg(feature = "use_std")]
         $i
     )*)
+}
+
+if_std!{
+    pub type BoxFuture<T, E> = std::boxed::Box<Future<Item = T, Error= E> + Send>;
+
+    impl <F: ?Sized + Future> Future for std::boxed::Box<F> {
+        type Item = F::Item;
+        type Error = F::Error;
+
+        fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+            (**self).poll()
+        }
+    }
 }
 
 pub trait Future {
@@ -26,4 +48,34 @@ pub trait Future {
     {
         panic!("Not implet yet");
     }
+
+    #[cfg(feature = "use_std")]
+    fn boxed(self) -> BoxFuture<Self::Item, Self::Error>
+    where
+        Self: Sized + Send + 'static,
+    {
+        ::std::boxed::Box::new(self)
+    }
+
+    fn map<F, U>(self, f: F) -> Map<Self, F>
+    where
+        F: FnOnce(Self::Item) -> U,
+        Self: Sized,
+    {
+        assert_future::<U, Self::Error, _>(map::new(self, f))
+    }
+
+    fn map_err<F, E>(self, f: F) -> MapErr<Self, F>
+    where
+        F: FnOnce(Self::Error) -> E,
+    {
+        assert_future::<Self::Item, E, _>(map_err::new(self, f))
+    }
+}
+
+fn assert_future<A, B, F>(t: F) -> F
+where
+    F: Future<Item = A, Error = B>,
+{
+    t
 }
---
+++ src/map_err.rs
@@ -0,0 +1,34 @@
+use {Async, Future, Poll};
+
+#[must_use = "futures do nothing unless polled"]
+pub struct MapErr<A, F>
+where
+    A: Future,
+{
+    future: A,
+    f: Option<F>,
+}
+
+pub fn new<A, F>(future: A, f: F) -> MapErr<A, F>
+where
+    A: Future,
+{
+    MapErr { future, f: Some(f) }
+}
+
+impl<U, A, F> Future for MapErr<A, F>
+where
+    A: Future,
+    F: FnOnce<A::Error>,
+{
+    type Item = A::Item;
+    type Error = U;
+
+    fn poll(&mut self) -> Poll<A::Item, U> {
+        let e = match self.future.poll() {
+            Ok(Async::NotReady) => return Ok(Async::NotReady),
+            other => other,
+        };
+        e.map_err(self.f.take().expect("cannot poll MapErr twice"))
+    }
+}
